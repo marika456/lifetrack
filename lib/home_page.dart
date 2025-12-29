0382af9e9a91ae0c3c1f02afba59a8d27b04a13f
Marika D'Acri <marikadacri@gmail.com>
2025-12-29 20:56:55 +0100

    Localizzazione multilingua(it/en) con file JSON e navigazione tramite TabBar

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,18 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:lifetrack/app_localizations.dart';
 
 class HomePage extends StatelessWidget{
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    int ora= DateTime.now().hour;
+    String chiave;
+
+    if(ora < 12){
+      chiave="welcome_morning";
+    } else if (ora < 18) {
+      chiave = "welcome_afternoon";
+    } else {
+      chiave = "welcome_evening";
+    }
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("LifeTrack - Home"),
+      body: Center(
+        child: Text(
+          AppLocalizations.of(context)!.translate(chiave),
+          style: const TextStyle(fontSize: 24),
+        ),
       ),
-      body: const Center(
-        child: Text("Benvenuta su LifeTrack!"),
-      )
     );
-    throw UnimplementedError();
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'constants.dart';
+import 'app_localizations.dart';
 import 'home_page.dart';
 import 'profile_page.dart';
 import 'tips_page.dart';
@@ -15,57 +18,48 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: .fromSeed(seedColor: Colors.lightBlueAccent),
-      ),
+      title: Constants.APP_NAME,
+      localizationsDelegates: [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('it', ''),
+        Locale('en', ''),
+      ],
       home: const MainScreen(),
     );
   }
 }
-class MainScreen extends StatefulWidget {
+
+class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
 
   @override
-  State<MainScreen> createState() => _MainScreenState();
-}
-
-class _MainScreenState extends State<MainScreen> {
-  int _currentIndex=0; //quale pagina è selezionata(0,1,2)
-  final List<Widget> _pages=[
-    const HomePage(),
-    const ProfilePage(),
-    const TipsPage(),
-  ];//lista delle pagine da mostrare
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      //mostra la pagina corrente presa dalla lista
-      body: _pages[_currentIndex],
-      //barra di navigazione in basso
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,//dice alla barra quale icona illuminare
-        onTap: (index){
-          //questa funzione scatta quando tocco l'icona
-          setState(() {
-            _currentIndex=index; //aggiorna indice e ridisegna app
-          });
-        },
-        items: const [
-          BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'Home',
+    return DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text(Constants.APP_NAME),
+            bottom: TabBar(
+                tabs: [
+                  Tab(icon: const Icon(Icons.home), text: AppLocalizations.of(context)!.translate('home')),
+                  Tab(icon: const Icon(Icons.lightbulb), text: AppLocalizations.of(context)!.translate('tips')),
+                  Tab(icon: const Icon(Icons.person), text: AppLocalizations.of(context)!.translate('profile')),
+                ],
+            ),
           ),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.account_circle),
-              label: 'Profilo',
+          body: const TabBarView(
+              children: [
+                HomePage(),
+                TipsPage(),
+                ProfilePage(),
+              ],
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.lightbulb),
-            label: 'Tips',
-          ),
-        ],
-      ),
+        ),
     );
   }
 }
+
